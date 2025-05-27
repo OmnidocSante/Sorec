4c3640e7892611dddcfaaac6a968d509a28b9c8f
AbdelbassetHmidchat1 <abdelbassethmidchat1@gmail.com>
2025-05-27 14:54:30 +0100

    fixed additional bugs

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Header() {
       <footer className="bg-bay-of-many-950 text-bay-of-many-100 py-8 px-4 md:px-8">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
-            <p className="text-sm">&copy; 2024 Sorec. Tous droits réservés.</p>
+            <p className="text-sm">&copy; 2025 Omnidoc. Tous droits réservés.</p>
           </div>
           
 
@@ -13,13 +13,13 @@ export default function Header() {
               href="mailto:hello@sorec.com"
               className="hover:text-bay-of-many-300 transition-colors"
             >
-              hello@sorec.com
+              support@omnidoc.ma
             </a>
             <a
               href="tel:+1234567890"
               className="hover:text-bay-of-many-300 transition-colors"
             >
-              +212 5 22222222
+              +212 5 22240404
             </a>
           </div>
         </div>
